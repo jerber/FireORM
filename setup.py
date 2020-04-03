c4b128dec59c7c6309ba531c348b77a1850d7423
Jeremy Berman <jerber@sas.upenn.edu>
2020-04-03 02:12:39 -0400

    adding setup

---
+++ setup.py
@@ -0,0 +1,64 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+setup(
+	name='fireorm',
+
+	# Versions should comply with PEP440.  For a discussion on single-sourcing
+	# the version across setup.py and the project code, see
+	# https://packaging.python.org/en/latest/single_source_version.html
+	version='0.0.1',
+
+	description="Firestore ORM for python.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+
+	# The project's main homepage.
+	url="https://github.com/jerber/FireORM",
+
+	# Author details
+	author="Jeremy",
+	author_email="kellycup8@gmail.com",
+
+	# Choose your license
+	license="Apache 2.0",
+
+	# See https://pypi.org/classifiers/
+	classifiers=[
+		# How mature is this project? Common values are
+		#   3 - Alpha
+		#   4 - Beta
+		#   5 - Production/Stable
+		'Development Status :: 3 - Alpha',
+
+		# Indicate who your project is intended for
+		"Intended Audience :: Developers",
+		"Operating System :: OS Independent",
+
+		# Pick your license as you wish (should match "license" above)
+		"License :: OSI Approved :: Apache Software License",
+
+		# Specify the Python versions you support here. In particular, ensure
+		# that you indicate whether you support Python 2, Python 3 or both.
+		"Programming Language :: Python",
+		"Programming Language :: Python :: 3",
+		"Programming Language :: Python :: 3.6",
+		"Programming Language :: Python :: 3.7"
+	],
+
+	# What does your project relate to?
+	keywords='Firestore Python ORM Models Firebase',
+
+	# Add all packages except tests
+	packages=find_packages('src', exclude=['tests']),
+
+	package_dir={'': 'src'},
+
+	# List run-time dependencies here.  These will be installed by pip when
+	# your project is installed. For an analysis of "install_requires" vs pip's
+	# requirements files see:
+	# https://packaging.python.org/en/latest/requirements.html
+	install_requires=['firebase-admin'],
+)
